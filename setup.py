dc5b25745ea4bbb7174d39f5d5adf62fb2fe3088
Igor Varga <ivarhauzh@gmail.com>
2021-04-27 20:54:30 +0200

    Create setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py_stn_segmentation',
+    version='1.1',
+    packages=['bayessian_appearance'],
+    url='',
+    license='',
+    author='ivarh',
+    author_email='ivarhauzh@gmail.com',
+    description=''
+)
